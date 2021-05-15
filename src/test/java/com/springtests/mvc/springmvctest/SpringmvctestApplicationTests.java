dbba47b35e055bd2c263f501b557c379289b5296
yirez <irezyigit@gmail.com>
2021-05-15 12:23:46 +0300

    Initial commit

---
+++ src/test/java/com/springtests/mvc/springmvctest/SpringmvctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtests.mvc.springmvctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvctestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
